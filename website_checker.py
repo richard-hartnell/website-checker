042bdbb592cfe6e9eec02bebe37cddf890b2a9af
unknown <richard@richardhartnell.com>
2023-07-27 21:44:13 -0700

    Test case achieved.

--- website_checker.py
+++ website_checker.py
@@ -1,56 +1,72 @@
 import time
 import hashlib
 from urllib.request import urlopen, Request
+import smtplib
+from email import message
 
-# setting the URL you want to monitor
-url1 = Request('https://www.artsfund.org/accelerator/',
-              headers={'User-Agent': 'Mozilla/5.0'})
-url2 = Request('https://www.whatcomcf.org', headers={'User-Agent': 'Mozilla/5.0'})
+def send_email(url):
+    msg = message.Message()
+    msg.add_header('from', 'pi@richardhartnell.com')
+    msg.add_header('to', 'programming@lookoutarts.com')
+    msg.add_header('subject', ('Website updated: ' + url))
+    msg.set_payload('Hello! A little computer would like to let you know that ' + url + ' was recently updated.')
+    server = smtplib.SMTP('smtp.dreamhost.com', 587)
+    server.login('pi@richardhartnell.com', pwd)
+    server.send_message(msg, from_addr='pi@richardhartnell.com', to_addrs='programming@lookoutarts.com')
 
-# to perform a GET request and load the
-# content of the website and store it in a var
-response = urlopen(url).read(9000)
+from_addr = 'pi@richardhartnell.com'
+to_addr = 'programming@lookoutarts.com'
+hashes = {}
 
-# to create the initial hash
-currentHash = hashlib.sha224(response).hexdigest()
+with open('./creds.txt', 'r') as password_file:
+    pwd = password_file.read()
+
+url_list = ['https://www.artsfund.org/accelerator/',
+            'https://www.whatcomcf.org',
+]
+
+#create initial hashes
+for url in url_list:
+    response = urlopen(Request(url, headers={'User-Agent': 'Mozilla/5.0'})).read(9000)
+    hashes[url] = hashlib.sha224(response).hexdigest()
+
+send_email('test.com')
 print("running")
+print(hashes)
 time.sleep(3)
-while True:
-    try:
-        # perform the get request and store it in a var
-        response = urlopen(url).read(9000)
 
-        # create a hash
-        currentHash = hashlib.sha224(response).hexdigest()
+# while True:
+#     for url in url_list:
+#         try:
+#             # perform the get request and store it in a var
+#             response = urlopen(url).read(9000)
 
-        # wait for 30 seconds
-        time.sleep(3)
+#             # create a hash
+#             currentHash = hashlib.sha224(response).hexdigest()
 
-        # perform the get request
-        response = urlopen(url).read(9000)
+#             # wait for 30 seconds
+#             time.sleep(3)
 
-        # create a new hash
-        newHash = hashlib.sha224(response).hexdigest()
+#             # perform the get request
+#             response = urlopen(url).read(9000)
 
-        # check if new hash is same as the previous hash
-        if newHash == currentHash:
-            continue
+#             # create a new hash
+#             newHash = hashlib.sha224(response).hexdigest()
 
-        # if something changed in the hashes
-        else:
-            # notify
-            print("something changed")
+#             # check if new hash is same as the previous hash
+#             if newHash == currentHash:
+#                 continue
 
-            # again read the website
-            response = urlopen(url).read(9000)
+#             # if something changed in the hashes
+#             else:
+#                 # notify
+#                 send_email(url)
 
-            # create a hash
-            currentHash = hashlib.sha224(response).hexdigest()
+#                 #save new hash
+#                 response = urlopen(url).read(9000)
+#                 hashes[url] = hashlib.sha224(response).hexdigest()
+#                 time.sleep(3)
+#                 continue
 
-            # wait for 30 seconds
-            time.sleep(3)
-            continue
-
-    # To handle exceptions
-    except Exception as e:
-        print("error")
+#         except Exception as e:
+#             print("error")
